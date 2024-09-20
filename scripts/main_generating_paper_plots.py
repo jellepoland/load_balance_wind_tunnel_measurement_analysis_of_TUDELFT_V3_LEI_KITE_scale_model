a3f7b8468a2d131e5e28fb9628d3122923760633
jellepoland <jellepoland@gmail.com>
2024-09-20 13:20:23 +0200

    changing file name and main script to include a run of the uncertainity table

--- scripts/main_generating_paper_plots.py
+++ scripts/main_generating_paper_plots.py
@@ -1,20 +1,18 @@
-from reynolds_variation_plots import main as reynolds_variation_plots_main
-from uncertainty_boxplots import main as uncertainty_boxplots_main
-from literature_comparison import main as literature_comparison_main
-from reynolds_variation_plots import defining_root_dir
 from pathlib import Path
+import reynolds_variation_plots
+import literature_comparison
+import uncertainty_table
 
 
 def main():
-    root_dir = defining_root_dir()
+    root_dir = reynolds_variation_plots.defining_root_dir()
     results_path = Path(root_dir) / "results"
     results_path = Path(
         "/home/jellepoland/ownCloud/phd/latex_documents/WES24_KITE_WindTunnel/Images"
     )
-
-    reynolds_variation_plots_main(results_path, root_dir)
-    uncertainty_boxplots_main(results_path, root_dir)
-    literature_comparison_main(results_path, root_dir)
+    reynolds_variation_plots.main(results_path, root_dir)
+    literature_comparison.main(results_path, root_dir)
+    uncertainty_table.main(root_dir)
 
     print(
         f"\n--> New plots generated in saved inside the latex-folder, located at: \n {results_path}"
